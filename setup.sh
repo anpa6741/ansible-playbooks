9255f75110f1c4c5ef80bf02f8b8b739f24b55ec
anpa6741 <anpa6641@colorado.edu>
2021-04-23 17:22:40 +0530

    refactored networking to play to add support for configuring multiple ips

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ vagrant ssh -c "sudo systemctl restart sshd"
 ssh-copy-id vagrant@127.0.0.1 -p 2222
 vagrant ssh -c "sudo sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config"
 vagrant ssh -c "sudo systemctl restart sshd"
-
+vagrant ssh -c "sudo apt update"
